ab23b52a887e3dec5db90080aa3d1cfb0edb79d8
Mustafa Mokashi <neyaadeez@gmail.com>
2024-04-20 00:51:42 -0700

    bookMarkLet

---
+++ bookmarklet.py
@@ -0,0 +1,10 @@
+#         javascript:(function() {
+#             var encryptedFlag = "àÒÆÞ¦È¬ëÙ£ÖÓÚåÛÑ¢ÕÓÓÇ¡¥Ìí";
+#             var key = "picoctf";
+#             var decryptedFlag = "";
+#             for (var i = 0; i < encryptedFlag.length; i++) {
+#                 decryptedFlag += String.fromCharCode((encryptedFlag.charCodeAt(i) - key.charCodeAt(i % key.length) + 256) % 256);
+#             }
+#             alert(decryptedFlag);
+#         })();
+# picoCTF{p@g3_turn3r_0c0d211f}
