65ecabfc4255d56e6ff568138fc174ce9daeffcc
Mustafa Mokashi <neyaadeez@gmail.com>
2024-04-20 03:56:13 -0700

    drop-in

---
+++ enc_flag.py
@@ -0,0 +1,6 @@
+import base64;
+code = 'YidkM0JxZGtwQlRYdHFhR3g2YUhsZmF6TnFlVGwzWVROclh6ZzJhMnd6TW1zeWZRPT0nCg=='
+output = base64.b64decode(code)
+# output = base64.b64decode(output)
+output = base64.b64decode('d3BqdkpBTXtqaGx6aHlfazNqeTl3YTNrXzg2a2wzMmsyfQ==')
+print(output)
